cd62a54ca0ebba1c65707b7d112b65ffcf59b10d
Ngoc <dungtuyetngoc@gmail.com>
2023-07-26 18:55:56 +0700

    fixed error button and add wifi

--- sketch.ino
+++ sketch.ino
@@ -1,75 +1,91 @@
 #include <LiquidCrystal_I2C.h>
 #include <ESP32Servo.h>
 #include <Keypad.h>
-#include "WiFi.h" // for ESP32
+#include "WiFi.h"         // for ESP32
 #include "PubSubClient.h" // MQTT
 
 // simulate connecting to internet through WIFI using ESP32
-const char* ssid = "Wokwi-GUEST";
-const char* password = "";
-const char* mqtt_server = "mqtt3.thingspeak.com"; // Thingspeak MQTT broker
-const char* pTopic = "web/switch"; 
+const char *ssid = "Wokwi-GUEST";
+const char *password = "";
+const char *mqtt_server = "broker.hivemq.com"; // Thingspeak MQTT broker
+int port = 1883;
+
+const char *pTopic = "web/switch";
 
 // wifi setup through TCP/IP
-WiFiClient espClient; 
-PubSubClient client(espClient); 
+WiFiClient espClient;
+PubSubClient client(espClient);
 
-
-#define ROW_NUM     4 
-#define COLUMN_NUM  4
-//buzzer
+#define ROW_NUM 4
+#define COLUMN_NUM 4
+// buzzer
 #define Buzzer_pin 23
-//button
-#define Button_01_pin 1
+// button
+#define Button_01_pin 16
 #define Button_02_pin 3
-//light
+// light
 #define light_sensor 36
 // Ultrasonic Distance Sensor
 #define trig_pin 4
 #define echo_pin 2
 #define servo_pin 15
-//RBG 01
-#define PIN_RED_01   26 // GIOP26
+// RBG 01
+#define PIN_RED_01 26   // GIOP26
 #define PIN_GREEN_01 33 // GIOP33
-#define PIN_BLUE_01 25 // GIOP25
+#define PIN_BLUE_01 25  // GIOP25
 // RBG 02
-#define PIN_RED_02 13 // GIOp13
+#define PIN_RED_02 13   // GIOp13
 #define PIN_GREEN_02 12 // GIOP12
-#define PIN_BLUE_02 14 // GIOP14
+#define PIN_BLUE_02 14  // GIOP14
 // lcd 16x2 i2c
-LiquidCrystal_I2C lcd(0x27,16, 2);  // lcd
+LiquidCrystal_I2C lcd(0x27, 16, 2); // lcd
 
 // keypad
 char keys[ROW_NUM][COLUMN_NUM] = {
-  { '1', '2', '3', 'A' },
-  { '4', '5', '6', 'B' },
-  { '7', '8', '9', 'C' },
-  { '*', '0', '#', 'D' }
-};
-byte pin_rows[ROW_NUM] = {17,5,18,19};
-byte pin_column[COLUMN_NUM] = {33,32,35,34};
-Keypad keypad = Keypad( makeKeymap(keys),  pin_rows,pin_column,ROW_NUM,COLUMN_NUM);
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}};
+byte pin_rows[ROW_NUM] = {17, 5, 18, 19};
+byte pin_column[COLUMN_NUM] = {33, 32, 35, 34};
+Keypad keypad = Keypad(makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM);
 
-
-void setupWifi(){
+void setupWifi()
+{
   Serial.println("Connecting to ");
   Serial.println(ssid);
 
-  lcd.
+  lcd.setCursor(0, 0);
+  lcd.print("Connecting to ");
+  lcd.setCursor(0, 1);
+  lcd.print("WIFI. ");
+
+  WiFi.begin(ssid, password);
+
+  // wait until wifi is connected
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("WiFi connected");
+
+  lcd.print("Connected");
 }
+
 // test
-void setup() {
-  Serial.begin(9600); 
+void setup()
+{
+  Serial.begin(9600);
+
   lcd.init();
   lcd.backlight();
 
+  setupWifi();
   // set up other devices down here
-
 }
 
-void loop() {
-  
+void loop()
+{
 }
-
-
-
